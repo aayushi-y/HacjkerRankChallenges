b6ca78d45fd4cc3954952b58903033dd6709870d
Ayushi Yadav <33841859+aayushi-y@users.noreply.github.com>
2024-02-01 11:07:12 +0530

    Update Basic-SQL-queries.sql

--- Basic-SQL-queries.sql
+++ Basic-SQL-queries.sql
@@ -5,3 +5,15 @@ select * from city where countrycode = 'USA' and population > 100000;
 /*Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.*/
 
 select name from city where countrycode = 'USA' and population > 120000;
+
+/*Query all columns (attributes) for every row in the CITY table.*/
+
+select * from city;
+
+/*Query all columns for a city in CITY with the ID 1661.*/
+
+select * from city where id = 1661;
+
+/*Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.*/
+
+select * from city where countrycode = 'JPN';
