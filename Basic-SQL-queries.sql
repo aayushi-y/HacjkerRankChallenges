d809fce89a41b4afbc2b731f9463b3c61f3e5bd4
Ayushi Yadav <33841859+aayushi-y@users.noreply.github.com>
2024-02-01 11:04:09 +0530

    Update Basic-SQL-queries.sql

--- Basic-SQL-queries.sql
+++ Basic-SQL-queries.sql
@@ -1,3 +1,7 @@
 /*Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.*/
 
 select * from city where countrycode = 'USA' and population > 100000;
+
+/*Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.*/
+
+select name from city where countrycode = 'USA' and population > 120000;
